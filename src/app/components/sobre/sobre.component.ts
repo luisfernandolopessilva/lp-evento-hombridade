5b726a2512db275a786ad5b4b1c89530a0275cb2
Luis Fernando Lopes Silva <lfldos@gmail.com>
2025-10-12 01:40:23 -0300

    feat: Implementado section sobre

--- src/app/components/sobre/sobre.component.ts
+++ src/app/components/sobre/sobre.component.ts
@@ -8,4 +8,6 @@ import { CommonModule } from '@angular/common';
   templateUrl: './sobre.html',
   styleUrls: ['./sobre.scss']
 })
-export class SobreComponent {}
+export class SobreComponent {
+  // Podemos adicionar lógica aqui se necessário
+}
